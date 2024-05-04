85a8950dcda9d17f78b6336cc1c0dcda51fd5d8d
prateek051 <153995225+prateek051@users.noreply.github.com>
2024-05-04 18:31:25 +0000

    variables

---
+++ 1_Basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 1444
+let accountEmail = "prateekbadiger486@gmail.com"
+var accountPassword = "35887"
+accountCity = "Bengaluru"
+
+// accountId = 5  // not allowed
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
