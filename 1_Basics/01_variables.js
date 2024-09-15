4480d620e2b944d644b4445c0c3fd86f686a4f9c
prateek051 <153995225+prateek051@users.noreply.github.com>
2024-09-15 17:22:10 +0000

    edited

--- 1_Basics/01_variables.js
+++ 1_Basics/01_variables.js
@@ -1,8 +1,8 @@
-const accountId = 1444
-let accountEmail = "prateekbadiger486@gmail.com"
-var accountPassword = "35887"
-accountCity = "Bengaluru"
+// const accountId = 1444
+// let accountEmail = "prateekbadiger486@gmail.com"
+// var accountPassword = "35887"
+// accountCity = "Bengaluru"
 
-// accountId = 5  // not allowed
-console.log(accountId);
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+// // accountId = 5  // not allowed
+// console.log(accountId);
+// console.table([accountId, accountEmail, accountPassword, accountCity]);
